0ac13df4eb62d0e9f9395360508ff8db2d26d9aa
sumin1007 <suminmusic99@gmail.com>
2020-05-06 09:20:24 +0900

    exam

---
+++ public/MidtermExam/mid1.js
@@ -0,0 +1,14 @@
+let a = [];
+
+for (let i = 0; i < 100; i++){
+    a[i] = Math.floor(Math.random() * 100 + 1);
+}
+
+for (let i = 0; i < 100; i++){
+    while (5 < (a[i] % 10) < 9){
+        a.splice(i, 1);
+    }
+}
+
+console.log(a);
+
